7108dc9701a105f12c900b0d1db801c350c70621
山地奥斯卡537 <wushaoquan666@outlook.com>
2024-03-16 14:13:43 +0000

    537工具箱（最新测试版本）项目文件
    Signed-off-by: 山地奥斯卡537 <wushaoquan666@outlook.com>

---
+++ 537Toolbox_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef 537TOOLBOX_PRIVATE_H
+#define 537TOOLBOX_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.0.0.0"
+#define VER_MAJOR	1
+#define VER_MINOR	0
+#define VER_RELEASE	0
+#define VER_BUILD	0
+#define COMPANY_NAME	"537 Studio"
+#define FILE_VERSION	"1.0.0.0"
+#define FILE_DESCRIPTION	"537Toolbox"
+#define INTERNAL_NAME	"537Toolbox"
+#define LEGAL_COPYRIGHT	"Copyright(C)537Studio.2023-2024.All rights reserved."
+#define LEGAL_TRADEMARKS	"537Toolbox Logo"
+#define ORIGINAL_FILENAME	"537Toolbox"
+#define PRODUCT_NAME	"537Toolbox"
+#define PRODUCT_VERSION	"1.0.0.0"
+
+#endif /*537TOOLBOX_PRIVATE_H*/
